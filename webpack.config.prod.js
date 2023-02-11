98da3c1334dbc53bd2fb3538ce7386c9b72c4ee5
AndrewPerepelkin <avp.tep@rambler.ru>
2023-02-11 19:03:42 +0300

    feat: added layout and styles

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,7 @@
-const { merge } = require('webpack-merge')
+const {merge} = require('webpack-merge')
 const commonConfig = require('./webpack.config.common')
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
-const TerserPlugin = require("terser-webpack-plugin")
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(commonConfig, {
   mode: 'production',
