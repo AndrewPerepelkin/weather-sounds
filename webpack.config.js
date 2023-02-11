5d83d4877ed7056ae3c8132aabd299eb1da18c07
AndrewPerepelkin <avp.tep@rambler.ru>
2023-02-11 04:59:31 +0300

    feat: installed copy plugin, added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin")
+
+const DIST_DIRECTORY = 'dist'
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: './index.js',
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, DIST_DIRECTORY),
     clean: true,
   },
   plugins: [new HtmlWebpackPlugin({
     template: path.resolve(__dirname, 'public/index.html')
-  })],
+  }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public/favicon.png'),
+          to: path.resolve(__dirname, DIST_DIRECTORY),
+        }
+    ]
+  })
+  ],
 }
